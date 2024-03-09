7cc2efab018ca3ffdc07f59b6b93f169603c95df
huxilu <162772780+huxilu@users.noreply.github.com>
2024-03-09 16:38:28 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,8 +1,8 @@
-from joblib import load
+import joblib
 import pandas as pd
 import numpy as np
 
 def get_prediction(features):
-    linear = load('linear_regression_model.joblib')
+    linear = joblib.load('linear_regression_model.joblib')
     pred = linear.predict(features)
     return pred[0]
